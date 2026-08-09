bcab6a5a7eedd18fe14b7c3fa1a6f8a94668d858
Deepa <gs2295483@gmail.com>
2026-08-10 00:57:16 +0530

    Update 04_Fibonacci_Series.py

--- Python/04_Fibonacci_Series.py
+++ Python/04_Fibonacci_Series.py
@@ -1,1 +1,13 @@
+n = int(input("Enter number of terms: "))
 
+first = 0
+second = 1
+
+print("Fibonacci Series:")
+
+for _ in range(n):
+    print(first, end=" ")
+
+    first, second = second, first + second
+
+print()
