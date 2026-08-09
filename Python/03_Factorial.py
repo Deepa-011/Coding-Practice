dabb0435b9b531ecc884006216bd83119f2e31b8
Deepa <gs2295483@gmail.com>
2026-08-10 00:53:18 +0530

    Update 03_Factorial.py

--- Python/03_Factorial.py
+++ Python/03_Factorial.py
@@ -1,1 +1,11 @@
+num = int(input("Enter a number: "))
 
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    factorial = 1
+
+    for i in range(1, num + 1):
+        factorial *= i
+
+    print(f"Factorial of {num} = {factorial}")
