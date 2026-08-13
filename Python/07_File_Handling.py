42ce4978ac9b5a231f8e1dbfe3b2d7622af01545
Deepa <gs2295483@gmail.com>
2026-08-13 13:03:39 +0530

    Update 07_File_Handling.py

--- Python/07_File_Handling.py
+++ Python/07_File_Handling.py
@@ -1,1 +1,17 @@
+with open("student.txt", "w") as file:
+    file.write("Name: Deepa\n")
+    file.write("Course: B.Tech CSE AI\n")
+    file.write("Semester: 4th\n")
 
+print("Data written successfully.")
+
+with open("student.txt", "r") as file:
+    data = file.read()
+
+print("\nFile Content:")
+print(data)
+
+with open("student.txt", "a") as file:
+    file.write("Subject: Python Programming\n")
+
+print("Data appended successfully.")
